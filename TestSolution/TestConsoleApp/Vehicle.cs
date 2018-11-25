2a748263b2eab6d916652f5ce710db6aaaa13cb0
AbxdHxcks <abed0158@edu.easj.dk>
2018-11-26 00:06:29 +0100

    Added regions

--- TestSolution/TestConsoleApp/Vehicle.cs
+++ TestSolution/TestConsoleApp/Vehicle.cs
@@ -6,14 +6,19 @@ namespace TestConsoleApp
 {
     class Vehicle
     {
+        #region Enum
         public enum Type { Sedan, Offroad, Hatchback, SUV, Convertible}
         public enum Make { VW, BMW, Audi, Mercedes, Peugeot, Ford}
+        #endregion
 
+        #region Properties
         public Type CarType { get; set; }
         public Make CarMake { get; set; }
         public int SerialNr { get; set; }
         public string Model { get; set; }
+        #endregion
 
+        #region Constructor
         public Vehicle(Type carType, Make carMake, int serialNr, string model)
         {
             CarType = carType;
@@ -21,10 +26,13 @@ namespace TestConsoleApp
             SerialNr = serialNr;
             Model = model;
         }
+        #endregion
 
+        #region ToString
         public override string ToString()
         {
             return $"{nameof(CarType)}: {CarType}, {nameof(CarMake)}: {CarMake}, {nameof(SerialNr)}: {SerialNr}, {nameof(Model)}: {Model}";
         }
+        #endregion
     }
 }
