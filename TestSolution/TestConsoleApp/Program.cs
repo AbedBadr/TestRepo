d0456e4e4517ca3209c47e810341f9245dab8822
AbxdHxcks <abed0158@edu.easj.dk>
2018-11-25 23:02:22 +0100

    Made person object and vehicle object in program.cs

--- TestSolution/TestConsoleApp/Program.cs
+++ TestSolution/TestConsoleApp/Program.cs
@@ -8,7 +8,9 @@ namespace TestConsoleApp
         {
             Console.WriteLine("Hello World!");
 
-            Person test = new Person(1, "Jarred");
+            Person test = new Person(Person.Genders.Male, 1, "Jarred", 25);
+            Vehicle vehicle = new Vehicle(Vehicle.Type.Hatchback, Vehicle.Make.VW, 3213341, "Golf 4");
+
         }
     }
 }
